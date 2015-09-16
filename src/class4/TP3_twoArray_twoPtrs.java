d86e45197a33498a6d10579062a59731f4a54d68
Xu Tian <tianxu08@gmail.com>
2015-09-16 01:04:57 -0700

    916

--- src/class4/TP3_twoArray_twoPtrs.java
+++ src/class4/TP3_twoArray_twoPtrs.java
@@ -1,10 +1,32 @@
 package class4;
 
+import java.util.Arrays;
+
 public class TP3_twoArray_twoPtrs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	public static int smallestDifference(int[] A, int[] B) {
+        // write your code here
+		// sort the arrays first. 
+		
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		int i = 0, j = 0;
+		int min = Integer.MAX_VALUE;
+		while(i < A.length && j < B.length) {
+			min = Math.min(min, Math.abs(A[i] - B[j]));
+			if (A[i] < B[j]) {
+				i ++;
+			} else {
+				j ++;
+			}
+		}
+		
+		return min;
+    }
 }
